5c0e240ec77aec1495544fd362fd0bd7feba4a0b
Emin Orhan <aeminorhan@gmail.com>
2019-06-26 12:24:18 -0400

    stylistic

--- train.py
+++ train.py
@@ -64,7 +64,7 @@ print('RNN test loss:', scores[0])
 print('RNN test accuracy:', scores[1])
 
 # save the results
-model_id = args.task + '_' + args.init + '_' + '%.2f'%args.init_scale + '_' + '%.6f'%args.lr + '_' + '%i'%args.rand_seed
+model_id = args.task + '_' + args.init + '_' + '%.2f' % args.init_scale + '_' + '%.6f' % args.lr + '_' + '%i' % args.rand_seed
 model.save(model_id + '.h5')
 np.savez(model_id + '.npz', {'hist_acc': hist.history['acc'],
                              'hist_loss': hist.history['loss'],
